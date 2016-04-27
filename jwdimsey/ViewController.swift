18fe5a2dcddaa564dc628424e849e106c672860f
kit305 <kit305@imach-55.cis.utas.edu.au>
2016-04-28 09:53:12 +1000

    Initial Commit

---
+++ jwdimsey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jwdimsey
+//
+//  Created by kit305 on 28/04/2016.
+//  Copyright © 2016 kit305. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
